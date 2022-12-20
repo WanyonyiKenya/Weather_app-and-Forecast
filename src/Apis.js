93d6565a6642f2a8a7fe8ad4577017ae6ea192e9
Brian Wanyonyi <115996838+WanyonyiKenya@users.noreply.github.com>
2022-12-20 11:34:47 +0300

    Update Apis.js

--- src/Apis.js
+++ src/Apis.js
@@ -11,4 +11,3 @@ export const GeoApi_URl = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 
 export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_KEY = "f4a55bb0507fde348872d34b3c119f35"
